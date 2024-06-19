54da77848c76bed0305b5a2fe98036df3b475c52
MarceloniGithub <marcelf8@outlook.com>
2024-06-19 19:46:27 +0200

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  
+</script>
+
+<div class="container">
+  <h1>Welcome to Archivement</h1>
+
+  <p>Archive your Achievements</p>
+</div>
+
+<style>
+  :root {
+    font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
+    font-size: 16px;
+    line-height: 24px;
+    font-weight: 400;
+
+    color: #0f0f0f;
+    background-color: #f6f6f6;
+  }
+
+  h1, p {
+    text-align: center;
+  }
+</style>
