6308ce6f8961865888928c7d7da06f304e75f9ab
MarceloniGithub <marcelf8@outlook.com>
2024-06-20 17:01:22 +0200

    added footer and component for entries

---
+++ src/components/entry.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+  export let title = ""
+</script>
+
+<div class="entry">
+  <h2 class="entry-title">{title}</h2>
+  <div class="content-preview">
+    <img src="src/assets/example_image.jpg">
+    <img src="src/assets/example_image.jpg">
+    <img src="src/assets/example_image.jpg">
+    <img src="src/assets/example_image.jpg">
+    <img src="src/assets/example_image.jpg">
+  </div>
+</div>
+
+<style>
+  .entry {
+    background-color: #7e7e7e;
+    padding: 10px;
+    margin: 10px;
+    border: 3px solid rgb(51, 51, 51);
+    text-align: center;
+    width: 75%;
+  }
+  .entry-title {
+    margin: 0;
+    padding-bottom: 10px;
+    border-bottom: 3px solid rgb(51, 51, 51);
+  }
+  .content-preview {
+    padding-top: 10px;
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: repeat(2, 1fr);
+    gap: 10px; /* Optional: Add space between the grid items */
+  }
+  .content-preview img {
+    width: 100%;
+    height: auto;
+    display: block;
+  }
+  .content-preview img:nth-child(1) {
+    grid-column: 1 / 3; /* Span 2 columns */
+    grid-row: 1 / 3;    /* Span 2 rows */
+  }
+  .content-preview img:nth-child(2) {
+    grid-column: 3 / 4;
+    grid-row: 1 / 2;
+  }
+  .content-preview img:nth-child(3) {
+    grid-column: 3 / 4;
+    grid-row: 2 / 3;
+  }
+  .content-preview img:nth-child(4) {
+    grid-column: 4 / 4;
+    grid-row: 1 / 2;
+  }
+  .content-preview img:nth-child(5) {
+    grid-column: 4 / 4;
+    grid-row: 2 / 3;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,19 @@
 <script lang="ts">
-  
+  import Entry from "../components/entry.svelte"
 </script>
 
 <div class="container">
   <h1>Welcome to Archivement</h1>
 
   <p>Archive your Achievements</p>
+
+  <div id="entry-list">
+    <Entry title="Example Title"/>
+  </div>
+
+  <div id="footer">
+    <img alt="Add Entry" id="add-archive-button" src="src/assets/icons/plus.svg">
+  </div>
 </div>
 
 <style>
@@ -15,11 +23,42 @@
     line-height: 24px;
     font-weight: 400;
 
-    color: #0f0f0f;
-    background-color: #f6f6f6;
+    color: white;
+    background-color: #494646;
   }
 
   h1, p {
     text-align: center;
+  }
+
+  #entry-list {
+    display: flex;
+    justify-content: center;
+    align-self: center;
+    width: 100%;
+  }
+
+  #footer {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    background-color: rgb(49, 45, 45);
+    align-items: center;
+    justify-content: center;
+    display: flex;
+    padding-bottom: 10px;
+    padding-top: 10px;
+  }
+
+  #add-archive-button {
+    height: 50px;
+    width: 50px;
+    border-radius: 50%;
+    border-color: black;
+    border-width: 4px;
+    border-style: solid;
+    box-sizing: border-box;
+    filter: invert(1);
   }
 </style>
