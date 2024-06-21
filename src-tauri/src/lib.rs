7ed531afeb733be87192495e1cf6e57113947bc8
MarceloniGithub <marcelf8@outlook.com>
2024-06-21 20:30:47 +0200

    entries are now being saved in the local app data folder

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,8 +1,45 @@
+use tauri::{AppHandle, Manager};
+use uuid::Uuid;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![])
+        .invoke_handler(tauri::generate_handler![create_entry])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn create_entry(app: AppHandle, title: String, goals: Vec<String>, description: String) {
+    println!("{}", format!("Creating entry with title: {}", title));
+    let goalsVec: Vec<Goal> = goals.iter().map(|goal| Goal {title: goal.to_owned(), completed: false}).collect();
+    save_entry_settings(app, title, goalsVec, description)
+}
+
+#[derive(serde::Serialize, serde::Deserialize)]
+struct EntrySettings {
+    title: String,
+    goals: Vec<Goal>,
+    description: String
+}
+#[derive(serde::Serialize, serde::Deserialize)]
+struct Goal {
+    title: String,
+    completed: bool
+}
+
+fn save_entry_settings(app: AppHandle, title: String, goals: Vec<Goal>, description: String) {
+    let app_data_dir = app.path().app_data_dir().expect("failed to get app data dir");
+    let id = Uuid::new_v4();
+    println!("{}", format!("Saving entry with id: {}", id.to_string()));
+
+    let entry_dir = app_data_dir.join(id.to_string());
+    println!("{}", format!("Entry dir: {:?}", entry_dir));
+    std::fs::create_dir_all(&entry_dir).expect("failed to create entry dir");
+    
+    let entry_settings = EntrySettings {title, goals, description};
+    let entry_settings_path = entry_dir.join("settings.json");
+    let entry_settings_json = serde_json::to_string_pretty(&entry_settings).expect("failed to serialize entry settings");
+    std::fs::write(entry_settings_path, entry_settings_json).expect("failed to write entry settings");
 }
--- src/routes/create/+page.svelte
+++ src/routes/create/+page.svelte
@@ -1,17 +1,29 @@
 <script lang="ts">
+  import {invoke} from '@tauri-apps/api/core';
   import GoalInput from "../../components/goalInput.svelte";
+  import { goto } from '$app/navigation';
   let goals: GoalInput[] = []
   function addGoal() {
     goals.forEach((goal) => {goal.$set({closable: false})})
     const goalInstance = new GoalInput({target: document.getElementById("goals-list") as HTMLElement, anchor: document.getElementById("add-goal-button") as HTMLElement, props: {closable: true, onRemove: () => {goals.splice(goals.indexOf(goalInstance), 1); goals.at(-1)?.$set({closable: true})}}})
     goals.push(goalInstance)
   }
+  function createEntry() {
+    let title = (document.getElementById("title-input") as HTMLInputElement).value
+    let goalValues = [...document.getElementsByClassName("goal-input")].map((goal) => (goal as HTMLInputElement).value)
+    let description = (document.getElementById("entry-description") as HTMLTextAreaElement).value
+
+    invoke("create_entry", {title, goals: goalValues, description})
+    goto("/")
+  }
 </script>
 
 <div id="create-entry-page">
   <h1>Create a new Entry</h1>
-
   <p>You can still edit these later</p>
+
+  <input id="title-input" type="text" placeholder="Enter a Title">
+
   <div id="goals-list">
     <GoalInput></GoalInput>
     <button on:click={addGoal} id="add-goal-button"/>
@@ -20,7 +32,7 @@
   <textarea id="entry-description" placeholder="Description" maxlength=128/>
 
   <div id="footer">
-    <button id="create-entry-button">Create Entry</button>
+    <button id="create-entry-button" on:click={createEntry}>Create Entry</button>
   </div>
 </div>
 
@@ -48,8 +60,10 @@
     background-color: white;
     border: 3px solid black;
     background-image: url("src/assets/icons/plus.svg");
-    width: 40px;
-    height: 40px;
+    width: 80px;
+    background-repeat: no-repeat;
+    background-position: center;
+    height: 35px;
   }
 
   #create-entry-page {
@@ -57,6 +71,9 @@
   }
 
   #goals-list {
+    padding: 0.5rem;
+    background-color: #494646;
+    box-shadow: #000000 0 0 0.75rem;
     margin: auto;
     align-items: center;
     width: 20rem;
@@ -78,7 +95,12 @@
     padding-top: 0.5rem;
   }
 
-  #create-entry-button {
+  #title-input {
+    font-size: x-large;
+    margin-bottom: 2rem;
+  }
+  #create-entry-button{
+    cursor: pointer;
     font-size: x-large;
   }
 </style>
