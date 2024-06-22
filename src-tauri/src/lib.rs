a67353548e4e9899ee86c495aae770c085890b8e
MarceloniGithub <marcelf8@outlook.com>
2024-06-22 16:46:30 +0200

    added read_entries command that reads all entry settings files and returns them

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -5,7 +5,7 @@ use uuid::Uuid;
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![create_entry])
+        .invoke_handler(tauri::generate_handler![create_entry, read_entries])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
@@ -13,8 +13,8 @@ pub fn run() {
 #[tauri::command]
 fn create_entry(app: AppHandle, title: String, goals: Vec<String>, description: String) {
     println!("{}", format!("Creating entry with title: {}", title));
-    let goalsVec: Vec<Goal> = goals.iter().map(|goal| Goal {title: goal.to_owned(), completed: false}).collect();
-    save_entry_settings(app, title, goalsVec, description)
+    let goals_vec: Vec<Goal> = goals.iter().map(|goal| Goal {title: goal.to_owned(), completed: false}).collect();
+    save_entry_settings(app, title, goals_vec, description)
 }
 
 #[derive(serde::Serialize, serde::Deserialize)]
@@ -31,10 +31,13 @@ struct Goal {
 
 fn save_entry_settings(app: AppHandle, title: String, goals: Vec<Goal>, description: String) {
     let app_data_dir = app.path().app_data_dir().expect("failed to get app data dir");
+    let entries_dir = app_data_dir.join("entries");
+    if !entries_dir.exists() {std::fs::create_dir_all(&entries_dir).expect("failed to create entries dir");}
+
     let id = Uuid::new_v4();
     println!("{}", format!("Saving entry with id: {}", id.to_string()));
 
-    let entry_dir = app_data_dir.join(id.to_string());
+    let entry_dir = entries_dir.join(id.to_string());
     println!("{}", format!("Entry dir: {:?}", entry_dir));
     std::fs::create_dir_all(&entry_dir).expect("failed to create entry dir");
     
@@ -42,4 +45,20 @@ fn save_entry_settings(app: AppHandle, title: String, goals: Vec<Goal>, descript
     let entry_settings_path = entry_dir.join("settings.json");
     let entry_settings_json = serde_json::to_string_pretty(&entry_settings).expect("failed to serialize entry settings");
     std::fs::write(entry_settings_path, entry_settings_json).expect("failed to write entry settings");
+}
+
+#[tauri::command]
+fn read_entries(app: AppHandle) -> Vec<EntrySettings> {
+    let app_data_dir = app.path().app_data_dir().expect("failed to get app data dir");
+    let entries_dir = app_data_dir.join("entries");
+    if !entries_dir.exists() {std::fs::create_dir_all(&entries_dir).expect("failed to create entries dir");}
+
+    std::fs::read_dir(entries_dir).expect("couldn't read entries")
+    .filter(|entry| {entry.as_ref().unwrap().path().is_dir()})
+    .map(|entry| {
+        let entry = entry.expect("failed to read entry");
+        let entry_settings_path = entry.path().join("settings.json");
+        let entry_settings_json: EntrySettings = serde_json::from_str(&std::fs::read_to_string(entry_settings_path).expect("failed to read entry settings")).expect("failed to deserialize entry settings");
+        return entry_settings_json;
+    }).collect()
 }
