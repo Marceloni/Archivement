49daad808eae09a28cc54ab3331918eb921ec24a
MarceloniGithub <marcelf8@outlook.com>
2024-06-23 18:48:24 +0200

    improved css for content edit page

--- src/routes/entry_content/[settings]/+page.svelte
+++ src/routes/entry_content/[settings]/+page.svelte
@@ -8,7 +8,8 @@
 <div id="content-edit-page">
     <h1 id="entry-title">{settings.title}</h1>
     <p id="entry-description">{settings.description}</p>
-    {#each settings.content as contentPiece}
+    <div id="content-pieces">
+        {#each settings.content as contentPiece}
         {#if contentPiece.type === "image"}<img src={contentPiece.path} alt="Image">{/if}
         {#if contentPiece.type === "text"}<p>{contentPiece.text}</p>{/if}
         {#if contentPiece.type === "video"}
@@ -22,15 +23,26 @@
         </audio>
         {/if}
     {/each}
+    </div>
 </div>
 <style>
     #content-edit-page {
         text-align: center;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
     }
     #entry-description {
         padding-bottom: 1rem;
         border-bottom: 3px rgb(36, 36, 36) solid;
-        margin-bottom: 1rem;
+        width: 40rem;
     }
     p {margin: 0px}
+    #content-pieces {
+        width: 30rem;
+    }
+    #content-pieces img, video, p, audio {
+        margin-top: 2rem;
+        max-width: 100%;
+    }
 </style>
